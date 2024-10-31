6db0b17ee732cc87fa167941e3aebda61c14587d
Leela <leelanennuru21@gmsail.com>
2024-10-31 13:40:04 +0530

    hd

--- src/main/java/HybridTestNGFramework/Datadrivenframework/App.java
+++ src/main/java/HybridTestNGFramework/Datadrivenframework/App.java
@@ -1,13 +1,12 @@
 package HybridTestNGFramework.Datadrivenframework;
 
-/**
- * Hello world!
- *
- */
+
+import java.util.Scanner;
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+    public static void main( String[] args ) {
+String s="leela";
+StringBuilder sb=new StringBuilder(s);
+sb.setCharAt(0, s.charAt(1));
+System.out.print(sb);
+}}
